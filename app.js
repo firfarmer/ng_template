0fa3ab90a2fa615f290ac937ef66abbd40af709b
firfarmer <milesu@13MBA-10245.local>
2015-06-30 19:20:23 -0700

    Second column

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 	app.controller('ProfileController', ['$scope', function($scope){
 		$scope.title = 'Profiles';
 		$scope.data = profiles;
+		$scope.roles = roles;
 	}]);
 
-	var profiles = [{Id:1,Name:'Profile1',Count:99},{Id:2,Name:'Profile2',Count:12}];
+	var profiles = [{Id:1,Name:'Sales',Count:99},{Id:2,Name:'Finance',Count:12}];
+	var roles = [{Id:1,Name:'VP North America',Count:3},{Id:2,Name:'Regional VP',Count:8},{Id:3,Name:'Account Exec',Count:32}];
 })(); 	
