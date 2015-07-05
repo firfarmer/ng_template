66fa30851e716e3ef30637f5e39a625c835ac468
firfarmer <milesu@13MBA-10245.local>
2015-07-05 08:18:15 -0700

    Removed $scope

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (function(){
 	var app = angular.module('myApp', ['smart-table']);	
 
-	app.controller('ProfileController', ['$scope', function($scope){
-		$scope.title = 'Profiles';
-		$scope.data = profiles;
-		$scope.roles = roles;
-	}]);
+	app.controller('ProfileController', function(){
+		this.title = 'Profiles';
+		this.data = profiles;
+		this.roles = roles;
+	});
 
 	var profiles = [{Id:1,Name:'Sales',Count:99},{Id:2,Name:'Finance',Count:12}];
 	var roles = [{Id:1,Name:'VP North America',Count:3},{Id:2,Name:'Regional VP',Count:8},{Id:3,Name:'Account Exec',Count:32}];
